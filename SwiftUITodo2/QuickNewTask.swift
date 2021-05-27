b6a80c0b3951980702db75ace171e6f478bcd31f
seiya endo <seiya.endo0830@gmail.com>
2021-05-28 06:39:31 +0900

    update: todo entity create logic to quick new task view

--- SwiftUITodo2/QuickNewTask.swift
+++ SwiftUITodo2/QuickNewTask.swift
@@ -11,8 +11,12 @@ struct QuickNewTask: View {
     let category: TodoEntity.Category
     
     @State var newTask: String = ""
+    @Environment(\.managedObjectContext) var viewContext
     
     fileprivate func addNewTask() {
+        TodoEntity.create(in: self.viewContext,
+                          category: self.category,
+                          task: self.newTask)
         self.newTask = ""
     }
 
@@ -26,10 +30,12 @@ struct QuickNewTask: View {
                 self.addNewTask()
             })
                 .textFieldStyle(RoundedBorderTextFieldStyle())
+                .foregroundColor(.black)
             Button(action: {
                 self.addNewTask()
             }) {
                 Text("追加")
+                    .foregroundColor(.blue)
             }
             Button(action: {
                 self.cancelTask()
@@ -42,7 +48,11 @@ struct QuickNewTask: View {
 }
 
 struct QuickNewTask_Previews: PreviewProvider {
+    static let context = (UIApplication.shared.delegate as! AppDelegate)
+        .persistentContainer.viewContext
+
     static var previews: some View {
         QuickNewTask(category: .ImpUrg_1st)
+            .environment(\.managedObjectContext, context)
     }
 }
