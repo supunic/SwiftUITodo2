6aa0a7b9b41d261f756c44a3b80cf6551ba71f74
seiya endo <seiya.endo0830@gmail.com>
2021-05-28 06:53:55 +0900

    update: new task view form

--- SwiftUITodo2/NewTask.swift
+++ SwiftUITodo2/NewTask.swift
@@ -8,13 +8,26 @@
 import SwiftUI
 
 struct NewTask: View {
+    @State var task: String = ""
+    @State var time: Date = Date()
+
     var body: some View {
         // 画面遷移の管理で使用する、NavigationLinkと一緒に
         // タイトルやボタンを追加できる
         NavigationView {
             // NavigationViewに対してモディファイアを直接指定するではなく、内包するviewに対して指定する
-            VStack {
-                Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            
+            // Form ... 指定することでデザインが変わる
+            Form {
+                TextField("タスクを入力", text: $task)
+                DatePicker(selection: $time, label: { Text("日時") })
+                Button(action: {}) {
+                    HStack(alignment: .center) {
+                        Image(systemName: "minus.circle.fill")
+                        Text("キャンセル")
+                    }
+                    .foregroundColor(.red)
+                }
             }
             .navigationBarTitle("タスクの追加")
         }
