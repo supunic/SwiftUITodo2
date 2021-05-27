98bd1dcae32d6b985763712bf74f5269fd588f35
seiya endo <seiya.endo0830@gmail.com>
2021-05-28 06:01:23 +0900

    update: todo deetail row view hide flg and testdata

--- SwiftUITodo2/TodoDetailRow.swift
+++ SwiftUITodo2/TodoDetailRow.swift
@@ -11,10 +11,13 @@ struct TodoDetailRow: View {
     // @stateのオブジェクト版
     // @ObservedObjectによって、変数が持つメンバに変更があれば画面に反映される
     @ObservedObject var todo: TodoEntity
+    var hideIcon = false
     
     var body: some View {
         HStack {
-            CategoryImage(TodoEntity.Category(rawValue: todo.category))
+            if !hideIcon {
+                CategoryImage(TodoEntity.Category(rawValue: todo.category))
+            }
             CheckBox(checked: Binding(get: {
                 // get: checkedの値を参照するとき → 自分が保持するIntの値を評価して、Boolで返却
                 self.todo.state == TodoEntity.State.done.rawValue
@@ -50,12 +53,34 @@ struct TodoDetailRow: View {
 struct TodoDetailRow_Previews: PreviewProvider {
     static var previews: some View {
         // DB操作を行うためのcontextを取得
-        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        let context = (UIApplication.shared.delegate as! AppDelegate)
+            .persistentContainer.viewContext
+
         let newTodo = TodoEntity(context: context)
         newTodo.task = "将来への人間関係づくり"
-        newTodo.state = TodoEntity.State.todo.rawValue
+        newTodo.state = TodoEntity.State.done.rawValue
         newTodo.category = 0
+        
+        let newTodo1 = TodoEntity(context: context)
+        newTodo1.task = "クレームへの対応"
+        newTodo1.category = 1
 
-        return TodoDetailRow(todo: newTodo)
+        let newTodo2 = TodoEntity(context: context)
+        newTodo2.task = "無意味な接待や付き合い"
+        newTodo2.category = 2
+
+        let newTodo3 = TodoEntity(context: context)
+        newTodo3.task = "長時間、必要以上の息抜き"
+        newTodo3.category = 3
+
+        return VStack(alignment: .leading) {
+            VStack {
+                TodoDetailRow(todo: newTodo)
+                TodoDetailRow(todo: newTodo, hideIcon: true)
+                TodoDetailRow(todo: newTodo1)
+                TodoDetailRow(todo: newTodo2)
+                TodoDetailRow(todo: newTodo3)
+            }.environment(\.managedObjectContext, context)
+        }
     }
 }
