c79d90a6fb49e1e9e7613f044b83d83b2740133a
seiya endo <seiya.endo0830@gmail.com>
2021-05-28 06:14:36 +0900

    update: content view design

--- SwiftUITodo2/ContentView.swift
+++ SwiftUITodo2/ContentView.swift
@@ -9,8 +9,28 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
+        VStack(spacing: 0) {
+            Color.tBackground
+                .edgesIgnoringSafeArea(.top)
+                .frame(height: 0)
+            UserView(image: Image("profile"), userName: "taro tanaka")
+            VStack(spacing: 0) {
+                HStack(spacing: 0) {
+                    CategoryView(category: .ImpUrg_1st)
+                    Spacer()
+                    CategoryView(category: .ImpNUrg_2nd)
+                }
+                Spacer()
+                HStack(spacing: 0) {
+                    CategoryView(category: .NImpUrg_3rd)
+                    Spacer()
+                    CategoryView(category: .NImpNUrg_4th)
+                }
+            }
             .padding()
+        }
+        .background(Color.tBackground)
+        .edgesIgnoringSafeArea(.bottom) // 下側だけ SafeArea を無視して表示
     }
 }
 
