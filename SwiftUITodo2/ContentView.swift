6b73ac9efc85224e799124475f9f0262dae216b1
seiya endo <seiya.endo0830@gmail.com>
2021-05-27 18:33:56 +0900

    Initial Commit

---
+++ SwiftUITodo2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUITodo2
+//
+//  Created by 遠藤聖也 on 2021/05/27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
