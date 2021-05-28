7cb5c77c9d208661e91d1c710672c77a59de8cbe
seiya endo <seiya.endo0830@gmail.com>
2021-05-28 10:01:06 +0900

    implement: edit task view

---
+++ SwiftUITodo2/EditTask.swift
@@ -0,0 +1,96 @@
+//
+//  EditTask.swift
+//  SwiftUITodo2
+//
+//  Created by 遠藤聖也 on 2021/05/28.
+//
+
+import SwiftUI
+
+struct EditTask: View {
+    // @ObservedObject ... TodoEntity の値を変更したらその変更がすぐDBに反映される
+    //                     （画面で入力したと同時に更新される、ただし永続化は別）
+    @ObservedObject var todo: TodoEntity
+    
+    // presentationMode ... view の表示制御に使用するオブジェクト（presentationMode に Binding された値として入る）
+    @Environment(\.presentationMode) var presentationMode
+    
+    var categries: [TodoEntity.Category] = [.ImpUrg_1st, .ImpNUrg_2nd, .NImpUrg_3rd, .NImpNUrg_4th]
+    @Environment(\.managedObjectContext) var viewContext
+    
+    // DBの永続化
+    fileprivate func save() {
+        do {
+            try self.viewContext.save()
+        } catch {
+            let nserror = error as NSError
+            fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+        }
+    }
+    
+    fileprivate func delete() {
+        viewContext.delete(todo)
+        save()
+    }
+
+    var body: some View {
+        // NavigationView は 画面遷移を開始するトップページに1つ定義しておけば良いため、ここでは書かない
+        Form {
+            Section(header: Text("タスク")) {
+                TextField("タスクを入力", text: Binding($todo.task, "new task"))
+            }
+            Section(header: Toggle(isOn: Binding(isNotNil: $todo.time,
+                                                 defaultValue: Date())) { Text("時間を指定する") }) {
+                if todo.time != nil {
+                    DatePicker(selection: Binding($todo.time, Date()), label: { Text("日時") })
+                } else {
+                    Text("時間未設定").foregroundColor(.secondary)
+                }
+            }
+            // 自動的に画面遷移が実装される ← SwiftUIの特徴
+            Picker(selection: $todo.category, label: Text("種類")) {
+//                    Text("重要かつ緊急").tag(0)
+//                    Text("重要だが緊急ではない").tag(1)
+//                    Text("重要でないが緊急").tag(2)
+//                    Text("重要でも緊急でもない").tag(3)
+                ForEach(categries, id: \.self) { category in
+                    HStack {
+                        CategoryImage(category)
+                        Text(category.toString())
+                    }
+                    .tag(category.rawValue)
+                }
+            }
+            Section(header: Text("操作")) {
+                Button(action: {
+                    // wip
+                }) {
+                    HStack(alignment: .center) {
+                        Image(systemName: "minus.circle.fill")
+                        Text("Delete")
+                    }
+                    .foregroundColor(.red)
+                }
+            }
+        }
+        .navigationBarTitle("タスクの編集")
+        .navigationBarItems(trailing: Button(action: {
+            self.save()
+            self.presentationMode.wrappedValue.dismiss()
+        }) { Text("閉じる") })
+    }
+}
+
+struct EditTask_Previews: PreviewProvider {
+    static let context = (UIApplication.shared.delegate as! AppDelegate)
+        .persistentContainer.viewContext
+
+    static var previews: some View {
+        let newTodo = TodoEntity(context: context)
+        
+        return NavigationView {
+            EditTask(todo: newTodo).environment(\.managedObjectContext, context)
+        }
+    }
+}
+
